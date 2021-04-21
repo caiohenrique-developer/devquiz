c9d2df4d6ff3733349be8df219ba88fba5a1ad97
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-20 23:27:51 -0300

    :chart_with_upwards_trend: [ WIP ]: Developing QuizCardView - the cards of the quiz

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widgets.dart';
 import 'package:flutter/material.dart';
 
 import 'widgets/level_button/level_button_widget.dart';
@@ -17,13 +18,33 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBarWidget(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            LevelButtonWidget(label: "Fácil"),
-            LevelButtonWidget(label: "Médio"),
-            LevelButtonWidget(label: "Difícil"),
-            LevelButtonWidget(label: "Perito"),
+            Padding(
+              padding: const EdgeInsets.only(top: 24, bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(label: "Fácil"),
+                  LevelButtonWidget(label: "Médio"),
+                  LevelButtonWidget(label: "Difícil"),
+                  LevelButtonWidget(label: "Perito"),
+                ],
+              ),
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 2,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
